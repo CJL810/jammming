390c27c990fe7a46491aebdfa07c989b6ec0a397
Cameron L <cam@Camerons-MacBook-Pro.local>
2017-10-16 18:39:08 -0400

    fixed small issue on spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -27,7 +27,7 @@ const Spotify = {
   search(term) {
     let accessToken = Spotify.getAccessToken();
     let endPointUrl = `https://api.spotify.com/v1/search?type=track&q=${term}`;
-    return fetch(`${endPointUrl}${term}`, {
+    return fetch(`${endPointUrl}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
